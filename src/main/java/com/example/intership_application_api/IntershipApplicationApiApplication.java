2f31b1c12c31ea9300ab82a5b42dcbe567408734
Michael The <72528@fif.co.id>
2025-09-08 19:31:43 +0700

    First Commit

---
+++ src/main/java/com/example/intership_application_api/IntershipApplicationApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.intership_application_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntershipApplicationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntershipApplicationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/intership_application_api/IntershipApplicationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intership_application_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntershipApplicationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
